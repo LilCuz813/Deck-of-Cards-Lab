e8210187ebec87b3f57af673871064cca72ce241
chris <chrisabelw@m3.com>
2023-02-13 15:37:17 -0500

    first commit

---
+++ Models/DeckDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Deck_of_Cards_API.Models
+{
+	public class DeckDAL
+	{
+        public static DeckModel GetDeck()//adjust here
+        {
+            //adjust here
+            //setup
+            
+            string url = "https://deckofcardsapi.com/api/deck/new/draw/?count=5";
+
+
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //adjust here
+            //Convert to C#
+            DeckModel result = JsonConvert.DeserializeObject<DeckModel>(JSON);
+            return result;
+
+        }
+    }
+}
+
